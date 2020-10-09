1590de78354282a1554a3db768b6faa9941980b8
DayanaBarquero <68917196+DayanaBarquero@users.noreply.github.com>
2020-10-08 22:45:42 -0600

    f

---
+++ src/Persona.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dayan on 8/10/2020.
+//
+
+#include "Persona.h"
---
+++ src/Persona.h
@@ -0,0 +1,19 @@
+//
+// Created by dayan on 8/10/2020.
+//
+
+#ifndef MY_PROJECT_NAME_PERSONA_H
+#define MY_PROJECT_NAME_PERSONA_H
+
+#include <iostream>
+class Persona {
+private:
+int id;
+std::string name;
+int age;
+public:
+
+};
+
+
+#endif //MY_PROJECT_NAME_PERSONA_H
